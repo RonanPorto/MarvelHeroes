4e33fd6b10273e8ddfc2590671831bef699bae0a
Ronan Porto <ronanportom@gmail.com>
2025-09-29 17:55:05 -0300

    Initial Commit

---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by ronan on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
